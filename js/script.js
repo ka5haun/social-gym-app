9d32097a274494bafbb8edaa8824745c43e7c4f5
ka5haun <kashaun.anderson@newberry.edu>
2025-12-07 23:37:14 -0500

    added vscode content

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("script.js connected!");
+document.addEventListener('click', function (e) {
+  if (
+    e.target.classList.contains('event-header') ||
+    e.target.classList.contains('dining-header') ||
+    e.target.classList.contains('contact-header') ||
+    e.target.classList.contains('card-header')
+  ) {
+    const card = e.target.closest('.event-card, .dining-card, .contact-card, .card');
+    if (card) {
+      card.classList.toggle('active');
+    }
+  }
+});
